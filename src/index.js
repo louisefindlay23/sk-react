24e86769763cf33f1a58207495212cb44bd3da94
Louise Findlay <louise.findlay@prismic.io>
2023-04-12 17:02:10 +0100

    Added Prismic toolbar

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
-import { PrismicProvider } from "@prismicio/react";
-import { client } from "./prismic";
+import { PrismicProvider, PrismicToolbar } from "@prismicio/react";
+import { client, repositoryName } from "./prismic";
 
 ReactDOM.render(
   <React.StrictMode>
     <PrismicProvider client={client}>
       <App />
+      <PrismicToolbar repositoryName={repositoryName} />
     </PrismicProvider>
   </React.StrictMode>,
   document.getElementById("root")
